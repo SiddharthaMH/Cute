7641205a56b34f0b2d1e160a0c7a0f93dfac0ac6
SiddharthaMH <150219930+SiddharthaMH@users.noreply.github.com>
2025-02-17 07:31:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js image optimization
   },
-  basePath: "/Cute", // Change 'your-repo' to your actual repository name
+  basePath: "/Cute", // Change 'Cute' to match your repository name
   assetPrefix: "/Cute",
 };
 export default nextConfig;
